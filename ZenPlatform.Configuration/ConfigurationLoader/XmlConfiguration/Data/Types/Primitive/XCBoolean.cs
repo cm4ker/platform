ed247b84c36a1b39224f0fc8d3c85b53ef594473
cm4ker <cm4ker@gmail.com>
2018-05-29 19:45:00 +0600

    stabilaze configuration api

---
+++ ZenPlatform.Configuration/ConfigurationLoader/XmlConfiguration/Data/Types/Primitive/XCBoolean.cs
@@ -0,0 +1,36 @@
+using System;
+using DBType = ZenPlatform.QueryBuilder.Schema.DBType;
+
+namespace ZenPlatform.Configuration.ConfigurationLoader.XmlConfiguration.Data.Types.Primitive
+{
+    public class XCBoolean : XCPremitiveType
+    {
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid(1, 2, 4, 56, 72, 234, 234, 23, 123, 12, 7);
+            }
+        }
+
+        public override string Name
+        {
+            get { return "Boolean"; }
+        }
+
+        public override bool IsNullable { get; set; }
+        public override int ColumnSize { get; set; }
+        public override DBType DBType
+        {
+            get { return DBType.Bit; }
+        }
+
+        public override Type CLRType
+        {
+            get { return (IsNullable) ? typeof(bool?) : typeof(bool); }
+        }
+        public override int Precision { get; set; }
+        public override int Scale { get; set; }
+
+    }
+}
---
+++ ZenPlatform.Configuration/ConfigurationLoader/XmlConfiguration/Data/Types/Primitive/XCNumeric.cs
@@ -0,0 +1,34 @@
+using System;
+using DBType = ZenPlatform.QueryBuilder.Schema.DBType;
+
+namespace ZenPlatform.Configuration.ConfigurationLoader.XmlConfiguration.Data.Types.Primitive
+{
+    public class XCNumeric : XCPremitiveType
+    {
+        public override Guid Id
+        {
+            get { return new Guid(1, 2, 4, 56, 72, 234, 234, 23, 123, 12, 5); }
+        }
+
+        public override string Name
+        {
+            get { return "Numeric"; }
+        }
+
+        public override bool IsNullable { get; set; }
+        public override int ColumnSize { get; set; }
+
+        public override DBType DBType
+        {
+            get { return DBType.Decimal; }
+        }
+
+        public override Type CLRType
+        {
+            get { return (IsNullable) ? typeof(decimal?) : typeof(decimal); }
+        }
+
+        public override int Precision { get; set; }
+        public override int Scale { get; set; }
+    }
+}
