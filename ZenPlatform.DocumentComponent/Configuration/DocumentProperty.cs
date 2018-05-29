1f2f06c74b0d7a5b2d395462d248779f511ff3b9
cm4ker <cm4ker@gmail.com>
2018-05-29 23:52:40 +0600

    stabilaze configuration api

---
+++ ZenPlatform.DocumentComponent/Configuration/DocumentProperty.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+using ZenPlatform.Configuration.ConfigurationLoader.Contracts;
+using ZenPlatform.Configuration.ConfigurationLoader.XmlConfiguration;
+using ZenPlatform.Configuration.ConfigurationLoader.XmlConfiguration.Data.Types.Primitive;
+using ZenPlatform.DataComponent.Configuration;
+
+namespace ZenPlatform.DocumentComponent.Configuration
+{
+    /// <summary>
+    /// Свойство документа
+    /// </summary>
+    public class DocumentProperty : XCObjectPropertyBase, IChildItem<Document>
+    {
+        private Document _parent;
+
+        public DocumentProperty()
+        {
+        }
+
+        [XmlIgnore] public Document Parent => _parent;
+
+        Document IChildItem<Document>.Parent
+        {
+            get => _parent;
+            set => _parent = value;
+        }
+    }
+}
