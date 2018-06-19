bf96cab5d0881e5569339ccdbccb3bdf4ae5893b
Nikita Zenkov <cmaker.xtal@hotmail.com>
2018-06-19 18:09:37 +0600

    fix

---
+++ ZenPlatform.QueryBuilder2/DDL/CreateTable/DropTableQueryNode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ZenPlatform.QueryBuilder2.Common;
+
+namespace ZenPlatform.QueryBuilder2.DDL.CreateTable
+{
+    public class DropTableQueryNode : SqlNode
+    {
+    }
+}
