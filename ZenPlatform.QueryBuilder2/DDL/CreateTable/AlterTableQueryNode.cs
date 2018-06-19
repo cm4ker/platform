82c9365cf47c34b69c5e2d52bfb4dfa74af345db
cm4ker <cm4ker@gmail.com>
2018-06-19 23:33:37 +0600

    sql impl insert

---
+++ ZenPlatform.QueryBuilder2/DDL/CreateTable/AlterTableQueryNode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ZenPlatform.QueryBuilder2.Common;
+using ZenPlatform.QueryBuilder2.DML.From;
+
+namespace ZenPlatform.QueryBuilder2.DDL.CreateTable
+{
+    public class AlterTableQueryNode : SqlNode
+    {
+        private TableNode _table;
+
+        public AlterTableQueryNode(string tableName, Action<TableNode> options)
+        {
+            _table = new TableNode(tableName);
+            options(_table);
+
+            Childs.AddRange(new SqlNode[]
+                {Tokens.AlterToken, Tokens.SpaceToken, Tokens.TableToken, Tokens.SpaceToken, _table});
+        }
+
+        public void AlterColumn()
+        {
+        }
+
+        public void Add()
+        {
+        }
+
+        public void Drop()
+        {
+        }
+    }
+}
--- ZenPlatform.QueryBuilder2/DDL/CreateTable/DropTableQueryNode.cs
+++ ZenPlatform.QueryBuilder2/DDL/CreateTable/DropTableQueryNode.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using ZenPlatform.QueryBuilder2.Common;
+using ZenPlatform.QueryBuilder2.DML.From;
 
 namespace ZenPlatform.QueryBuilder2.DDL.CreateTable
 {
     public class DropTableQueryNode : SqlNode
     {
+        private TableNode _table;
+
+        public DropTableQueryNode(string tableName) : this(tableName, t => { })
+        {
+        }
+
+        public DropTableQueryNode(string tableName, Action<TableNode> options)
+        {
+            _table = new TableNode(tableName);
+            options(_table);
+
+            Childs.AddRange(new SqlNode[]
+                {Tokens.DropToken, Tokens.SpaceToken, Tokens.TableToken, Tokens.SpaceToken, _table});
+        }
     }
 }
