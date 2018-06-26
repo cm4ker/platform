972b588c1d1f87c00f478db625dd20d4d0786172
cm4ker <cm4ker@gmail.com>
2018-06-26 21:44:02 +0600

    Merge branch 'master' of https://cmaker.visualstudio.com/_git/ZenPlatform
    # Conflicts:
    #	README.md

---
+++ ZenPlatform.Configuration/ConfigurationLoader/Structure/XCAttach.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace ZenPlatform.Configuration.ConfigurationLoader.XmlConfiguration
+{
+    public class XCAttach
+    {
+        [XmlAttribute]
+        public XCRefMode Mode { get; set; }
+
+        [XmlAttribute]
+        public Guid DestenationId { get; set; }
+    }
+}
