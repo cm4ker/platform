1c059ec97b11e9327e1e1a64b9b23d0fd4c5c389
cm4ker <cm4ker@gmail.com>
2018-06-27 21:47:23 +0600

    Merge branch 'master' of https://cmaker.visualstudio.com/_git/ZenPlatform
    # Conflicts:
    #	README.md

---
+++ ZenPlatform.ServerManagementTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZenPlatform.ServerManagementTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
