24836a6ba5e4adffa143fb47b90354307b905d12
Nikita Zenkov <cmaker.xtal@hotmail.com>
2018-08-05 02:59:43 +0600

    Add QueryUIBuilder project

---
+++ ZenPlatform.QueryUIBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZenPlatform.QueryUIBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
