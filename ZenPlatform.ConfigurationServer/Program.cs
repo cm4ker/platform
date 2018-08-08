67550a7002f618ca30e2e747aacb543e97990264
Nikita Zenkov <cmaker.xtal@hotmail.com>
2018-08-08 10:57:07 +0600

    Initial server conf impl

--- ZenPlatform.ConfigurationServer/Program.cs
+++ ZenPlatform.ConfigurationServer/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Text;
+using MessagePack;
+using NetMQ;
+using NetMQ.Sockets;
 
 namespace ZenPlatform.ConfigurationServer
 {
@@ -6,7 +10,40 @@ namespace ZenPlatform.ConfigurationServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var server = new ResponseSocket("@tcp://localhost:5556")) // bind
+            using (var client = new RequestSocket(">tcp://localhost:5556"))  // connect
+            {
+
+                var data = MessagePack.MessagePackSerializer.Serialize(new TestMessage() { MsgType = typeof(TestMessage), Hello = "OK", SomeContet = new TestMessage() { Hello = "NASTED" } });
+
+                client.SendFrame(data);
+
+                // Receive the message from the server socket
+                byte[] m1 = server.ReceiveFrameBytes();
+
+                Message message = MessagePack.MessagePackSerializer.Deserialize<TestMessage>(m1);
+
+                Console.WriteLine("From Client: {0}", message.MsgType);
+
+                Console.ReadKey();
+            }
         }
+    }
+
+    [MessagePackObject]
+    public class Message
+    {
+        [Key(0)]
+        public Type MsgType { get; set; }
+    }
+
+    [MessagePackObject]
+    public class TestMessage : Message
+    {
+        [Key(1)]
+        public string Hello { get; set; }
+
+        [Key(2)]
+        public TestMessage SomeContet { get; set; }
     }
 }
