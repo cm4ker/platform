23be9b40b793daf03c6c0cbc23ca2597b6a0af76
Nikita Zenkov <cmaker.xtal@hotmail.com>
2018-08-14 17:59:02 +0600

    add new ui elements

---
+++ ZenPlatform.UIGeneration2/Interface/UILabel.cs
@@ -0,0 +1,11 @@
+﻿namespace ZenPlatform.UIBuilder.Interface
+{
+    public class UILabel : UINode
+    {
+        public UILabel(string text = "")
+        {
+            Text = text;
+        }
+        public string Text { get; set; }
+    }
+}
