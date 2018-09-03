b5b90fc89b58a8285fb125be353f5d43a4cfb683
Nikita Zenkov <cmaker.xtal@hotmail.com>
2018-09-03 22:26:09 +0600

    refactor messages

--- ZenPlatform.EntityComponent/Configuration/SingleEntityConfigurationLoader.cs
+++ ZenPlatform.EntityComponent/Configuration/SingleEntityConfigurationLoader.cs
@@ -4,6 +4,7 @@ using System.Xml.Serialization;
 using ZenPlatform.Configuration.Data.Contracts;
 using ZenPlatform.Configuration.Structure;
 using ZenPlatform.Configuration.Structure.Data;
+using ZenPlatform.Configuration.Structure.Data.Types.Complex;
 using ZenPlatform.DataComponent.Configuration;
 using ZenPlatform.Shared.ParenChildCollection;
 
@@ -26,10 +27,37 @@ namespace ZenPlatform.EntityComponent.Configuration
                 var ser = new XmlSerializer(typeof(XCSingleEntityRule));
                 var rule = ser.Deserialize(sr) as XCSingleEntityRule ?? throw new Exception();
 
-                ((IChildItem<XCDataRuleContent>) rule).Parent = content;
+                ((IChildItem<XCDataRuleContent>)rule).Parent = content;
 
                 return rule;
             }
+        }
+    }
+
+    public class SingleEntityConfigurationManager : ConfigurationManagerBase
+    {
+        public SingleEntityConfigurationManager(XCComponent component) : base(component)
+        {
+        }
+
+        public override XCObjectTypeBase Create(XCObjectTypeBase parentType = null)
+        {
+            var newItem = new XCSingleEntity();
+
+            ((IChildItem<XCComponent>)newItem).Parent = Component;
+            Component.Parent.RegisterType(newItem);
+
+            return newItem;
+        }
+
+        /// <inheritdoc />
+        public override void Delete(XCObjectTypeBase type)
+        {
+           //TODO: Сделать удаление компонента. Для этого необходимо сделать следующие задачи
+           // 1) Проверка ссылочной целостности по объектам, т.е. узнать, нет ли ссылки на этот объект в других объектах
+           // 2) Проверка целостности кода платформы, т.е. узнать, не используется ли этот компонент где-то в коде
+           // Если одно  из  вышеперечисленных условий не выполнено, в таком случае нельзя давать удалять объект, а вывести список объектов, блокирующих удаление
+
         }
     }
 }
