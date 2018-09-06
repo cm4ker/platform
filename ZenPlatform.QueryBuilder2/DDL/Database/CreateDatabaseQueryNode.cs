8d2ae8a2451792588b7674832aa4e11449effc7b
Nikita Zenkov <cmaker.xtal@hotmail.com>
2018-09-06 13:05:04 +0600

    create intdex add

--- ZenPlatform.QueryBuilder2/DDL/Database/CreateDatabaseQueryNode.cs
+++ ZenPlatform.QueryBuilder2/DDL/Database/CreateDatabaseQueryNode.cs
@@ -35,32 +35,4 @@ namespace ZenPlatform.QueryBuilder.DDL.CreateDatabase
     {
         SqlNode Name { get; }
     }
-
-
-    public class DropDatabaseQueryNode : SqlNode, IDropDatabaseQuery
-    {
-        private SqlNode _name;
-
-        public DropDatabaseQueryNode()
-        {
-        }
-
-        public DropDatabaseQueryNode(string databaseName)
-        {
-            _name = new IdentifierNode(databaseName);
-        }
-
-        public DropDatabaseQueryNode WithName(string databaseName)
-        {
-            _name = new IdentifierNode(databaseName);
-            return this;
-        }
-
-        SqlNode IDropDatabaseQuery.Name => _name;
-    }
-
-    public interface IDropDatabaseQuery : IChildItem<Node>, IParentItem<Node, Node>
-    {
-        SqlNode Name { get; }
-    }
 }
