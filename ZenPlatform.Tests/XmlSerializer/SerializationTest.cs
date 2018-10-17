70f5ba75a9043b0f74e0056a36e32e4838701378
Nikita Zenkov <cmaker.xtal@hotmail.com>
2018-10-18 00:15:58 +0600

    XML Serialization

--- ZenPlatform.Tests/XmlSerializer/SerializationTest.cs
+++ ZenPlatform.Tests/XmlSerializer/SerializationTest.cs
@@ -1,12 +1,23 @@
+using System.IO;
+using Xunit;
+using ZenPlatform.Tests.XmlSerializer.TestedObjects;
+
 namespace ZenPlatform.Tests.XmlSerializer
 {
     public class SerializationTest
     {
+        private ZenPlatform.XmlSerializer.XmlSerializer _serializer = new ZenPlatform.XmlSerializer.XmlSerializer();
+
+        [Fact]
         public void SimpleObject()
         {
-            
+            var instance = new SimpleObjectWithNastedObject();
+            using (var tw = new StringWriter())
+            {
+                {
+                    _serializer.Serialize(instance, tw);
+                    var result = tw.ToString();
+                }
+            }
         }
     }
-    
-    
-}
