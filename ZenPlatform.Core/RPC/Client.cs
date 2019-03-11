2124a78fd41c6890ee66cc5e045ae0d4ecc7d6ee
Oleg Masterskikh <masterskih.oleg@gmail.com>
2019-03-11 18:02:11 +0600

    add namespace ZendPlatform.Core.RPC

---
+++ ZenPlatform.Core/RPC/Client.cs
@@ -0,0 +1,68 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZenPlatform.Core.RPC
+{
+    public class Client
+    {
+        private readonly Channel _channel;
+        private DefaultCallInvoker _invoker;
+        private readonly ISerializer _serializer;
+
+        internal Client(Channel channel, ISerializer serializer)
+        {
+            
+            _channel = channel;
+            _invoker = new DefaultCallInvoker(channel);
+            _serializer = serializer;
+
+
+        }
+        public async Task<object> Invoke(Route route, object @this, object[] parameters)
+        {
+
+            Request request = new Request()
+            {
+                patameters = parameters,
+                @this = @this
+            };
+
+            var callOptions = new CallOptions();
+            using (var call = _invoker.AsyncUnaryCall<Request, Responce>(
+                MethodDeclaration.GetMethod<Request, Responce>(route, _serializer), 
+                null, callOptions, request))
+            {
+                var result = await call.ResponseAsync.ConfigureAwait(false);
+
+                if (result.@this != null)
+                    CopyAll(result.@this, @this);
+
+                return result.result;
+                 
+            }
+            
+        }
+
+        private void CopyAll(object source, object target)
+        {
+           
+            var type = source.GetType();
+
+            if (type != target.GetType()) throw new ArgumentException("Параметры должны быть одного типа");
+
+            foreach (var sourceProperty in type.GetProperties())
+            {
+                var targetProperty = type.GetProperty(sourceProperty.Name);
+                targetProperty.SetValue(target, sourceProperty.GetValue(source, null), null);
+            }
+            foreach (var sourceField in type.GetFields())
+            {
+                var targetField = type.GetField(sourceField.Name);
+                targetField.SetValue(target, sourceField.GetValue(source));
+            }
+        }
+    }
+}
---
+++ ZenPlatform.Core/RPC/RouteFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ZenPlatform.Core.RPC
+{
+    public class RouteFactory
+    {
+        private RouteFactory() { }
+
+        public static RouteFactory Instance { get; } = new RouteFactory();
+
+        public Route GetRoute<T>(string methodName)
+        {
+            return new Route(typeof(T).FullName, methodName);
+        }
+
+        public Route GetRoute(MethodInfo method)
+        {
+            return new Route(method.DeclaringType.FullName, method.Name);
+        }
+
+        public Route GetRoute(string serviceName, string methodName)
+        {
+            return new Route(serviceName, methodName);
+        }
+    }
+}
