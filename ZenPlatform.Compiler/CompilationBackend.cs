d0853d919fdb4307222067a2d56873fe14ad18df
cm4ker <cm4ker@gmail.com>
2019-04-23 20:56:52 +0600

    console utils

---
+++ ZenPlatform.Compiler/CompilationBackend.cs
@@ -0,0 +1,63 @@
+using System.IO;
+using System.Threading;
+using Antlr4.Runtime;
+using Mono.Cecil;
+using ZenPlatform.Language;
+using ZenPlatform.Language.AST;
+using ZenPlatform.Language.AST.Definitions;
+using ZenPlatform.Language.Generation;
+
+namespace ZenPlatform.Compiler
+{
+    public class CompilationBackend
+    {
+        /// <summary>
+        /// Скомпилировать поток символов и записать в сборку
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="assemblyDefinition"></param>
+        public void Compile(Stream input, AssemblyDefinition assemblyDefinition)
+        {
+            var pTree = Parse(input);
+            ZLanguageVisitor v = new ZLanguageVisitor();
+            var module = v.VisitEntryPoint(pTree.entryPoint()) as Module;
+            Generator g = new Generator(module, assemblyDefinition);
+            g.Emit();
+        }
+
+
+        /// <summary>
+        /// Скомпилировать поток символов в сборку
+        /// </summary>
+        /// <param name="input">Входящий поток символов</param>
+        /// <param name="output">Поток для записи сборки</param>
+        /// <param name="and">Имя сборки</param>
+        /// <param name="moduleName">Имя модуля сборки</param>
+        /// <param name="mk">Тип модуля сборки</param>
+        public void Compile(Stream input, Stream output, AssemblyNameDefinition and, string moduleName, ModuleKind mk)
+        {
+            var assembly = AssemblyDefinition.CreateAssembly(and, moduleName, mk);
+
+            Compile(input, assembly);
+
+            assembly.Write(output);
+        }
+
+        /// <summary>
+        /// Распарсить исходный текст модуля
+        /// </summary>
+        /// <param name="input">Входящий поток символов</param>
+        /// <returns></returns>
+        private ZSharpParser Parse(Stream input)
+        {
+            AntlrInputStream inputStream = new AntlrInputStream(input);
+            ZSharpLexer lexer = new ZSharpLexer(inputStream);
+            CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
+            ZSharpParser parser = new ZSharpParser(commonTokenStream);
+
+            parser.AddErrorListener(new Listener());
+
+            return parser;
+        }
+    }
+}
