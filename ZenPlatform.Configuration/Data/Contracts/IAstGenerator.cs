441aa7c872a7dc72ea9b4c33e7bb211859c741d2
cm4ker <cm4ker@gmail.com>
2019-06-07 18:02:26 +0600

    refactor ast moving to the another file

--- ZenPlatform.Configuration/Data/Contracts/IAstGenerator.cs
+++ ZenPlatform.Configuration/Data/Contracts/IAstGenerator.cs
@@ -1,7 +1,9 @@
+using ZenPlatform.Language.Ast.AST.Builder;
+
 namespace ZenPlatform.Configuration.Data.Contracts
 {
     public interface IAstGenerator
     {
-        void Build 
+        void Build(AstBuilder builder);
     }
 }
