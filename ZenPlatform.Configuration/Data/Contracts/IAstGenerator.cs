23df60a677526a00855e564472765d44f4f11cf8
cm4ker <cm4ker@gmail.com>
2019-07-29 18:37:44 +0600

    Make component great again!

--- ZenPlatform.Configuration/Data/Contracts/IAstGenerator.cs
+++ ZenPlatform.Configuration/Data/Contracts/IAstGenerator.cs
@@ -1,9 +1,7 @@
-using ZenPlatform.Language.Ast.AST.Builder;
-
 namespace ZenPlatform.Configuration.Data.Contracts
 {
     public interface IAstGenerator
     {
-        void Build(AstBuilder builder);
+        //void Build(AstBuilder builder);
     }
 }
