a4891d9a37dd13816f00f30e9ddce11b13c65b53
cm4ker <cm4ker@gmail.com>
2019-07-30 14:13:43 +0600

    Simple code gen for SingleEntityComponent. Add contracts for code generation.

--- ZenPlatform.ConfigurationDataComponent/DataComponentBase.cs
+++ ZenPlatform.ConfigurationDataComponent/DataComponentBase.cs
@@ -56,7 +56,7 @@ namespace ZenPlatform.DataComponent
         public IEntityManager Manager { get; protected set; }
         public IXComponentManager ComponentManager { get; protected set; }
         public IEntityGenerator Generator { get; protected set; }
-        public IAstGenerator AstGenerator { get; protected set; }
+        public IPlatformStagedAssemblyGenerator AstGenerator { get; protected set; }
         public IDatabaseObjectsGenerator DatabaseObjectsGenerator { get; protected set; }
         public IEntityMigrator Migrator { get; protected set; }
         public IQueryInjector QueryInjector { get; protected set; }
