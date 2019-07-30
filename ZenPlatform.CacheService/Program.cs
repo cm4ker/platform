66a9a43bbb37d8ba170c8a3a53fbd728e1fff539
cm4ker <cm4ker@gmail.com>
2019-07-30 18:00:10 +0600

    Simple code gen for SingleEntityComponent. Add contracts for code generation.

---
+++ ZenPlatform.CacheService/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ServiceStack.Redis;
+
+
+namespace ZenPlatform.CacheService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var manager = new RedisManagerPool("localhost:6379");
+            using (var client = manager.GetClient())
+            {
+                client.Set("foo", "bar");
+            }
+        }
+    }
+}
