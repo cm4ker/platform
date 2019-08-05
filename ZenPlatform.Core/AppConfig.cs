feb3dc171c23626485ca0bede8513d1138b55a44
cm4ker <cm4ker@gmail.com>
2019-08-05 16:29:14 +0600

    add cache service config

--- ZenPlatform.Core/AppConfig.cs
+++ ZenPlatform.Core/AppConfig.cs
@@ -7,14 +7,18 @@ namespace ZenPlatform.Core
     public class AppConfig
     {
         public AccessPointConfig AccessPoint { get; set; }
+
+        public CacheServiceConfig CacheService { get; set; }
         public List<StartupConfig> Environments { get; set; }
+    }
+
+    public class CacheServiceConfig
+    {
+        public string Address { get; set; }
     }
 
     public class AccessPointConfig
     {
         public string Address { get; set; }
     }
-    
-
-    
 }
