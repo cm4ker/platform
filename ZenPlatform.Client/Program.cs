9a9ab2351558c042da5f4e96e31e76b0d255888d
cm4ker <cm4ker@gmail.com>
2019-10-17 11:21:17 +0600

    [client] Init work for client

---
+++ ZenPlatform.Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using ZenPlatform.Core.Assemlies;
+using ZenPlatform.Core.ClientServices;
+using ZenPlatform.Core.Logging;
+using ZenPlatform.Core.Network;
+
+namespace ZenPlatform.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the console client ZenPlatform");
+
+            Console.WriteLine("Please enter the destination address or leave empty for the default config [{0}] ",
+                "Some Addr");
+            
+            Console.WriteLine("Attempt to connect");
+            
+            
+            
+        }
+        
+        
+        public static ServiceProvider GetClientService()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+
+            services.AddSingleton<PlatformClient>();
+            services.AddSingleton<IClient, Core.Network.Client>();
+            services.AddTransient(typeof(ILogger<>), typeof(NLogger<>));
+            services.AddSingleton<PlatformAssemblyLoadContext>();
+            services.AddSingleton<IClientAssemblyManager, ClientAssemblyManager>();
+            services.AddTransient<ITransportClientFactory, TCPTransportClientFactory>();
+
+
+            services.AddSingleton(factory =>
+            {
+                var client = factory.GetRequiredService<IClient>();
+                return client.GetService<IAssemblyManagerClientService>();
+            });
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
