367c33301c5d0bb5161f382411ce664d7867ae14
Nikita Zenkov <cmaker.xtal@hotmail.com>
2019-10-27 14:08:33 +0600

    [test] separate file for ClientServerTestBase

--- ZenPlatform.Core.Test/UnitTestServerClient.cs
+++ ZenPlatform.Core.Test/UnitTestServerClient.cs
@@ -33,37 +33,6 @@ namespace ZenPlatform.Core.Test
     public delegate void InvokeInClientServerContextDelegate(ServiceProvider clientService,
         ServiceProvider serverSerice, ClientPlatformContext clientContext);
 
-    public class ClientServerTestBase
-    {
-        public void InvokeInClientServerContext(InvokeInClientServerContextDelegate action)
-        {
-            var serverServices = Initializer.GetServerService();
-            var clientServices = Initializer.GetClientService();
-
-
-            var environmentManager = serverServices.GetRequiredService<IPlatformEnvironmentManager>();
-            Assert.NotEmpty(environmentManager.GetEnvironmentList());
-
-
-            var accessPoint = serverServices.GetRequiredService<IAccessPoint>();
-            accessPoint.Start();
-            //need check listing
-
-            var platformClient = clientServices.GetRequiredService<ClientPlatformContext>();
-            platformClient.Connect(new Settings.DatabaseConnectionSettings()
-                {Address = "127.0.0.1:12345", Database = "Library"});
-            //need check connection
-
-            platformClient.Login("admin", "admin");
-            var assembly = platformClient.LoadMainAssembly();
-            Assert.NotNull(assembly);
-
-            action(clientServices, serverServices, platformClient);
-
-            accessPoint.Stop();
-        }
-    }
-
     public class UnitTestServerClient : ClientServerTestBase
     {
         [Fact]
