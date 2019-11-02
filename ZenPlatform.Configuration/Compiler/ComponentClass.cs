ab9ee233130de87d8318675e542df7c42d24eae3
cm4ker <cm4ker@gmail.com>
2019-11-03 00:19:38 +0600

    [compiler] ComponentObject & ComponentModule

---
+++ ZenPlatform.Configuration/Compiler/ComponentClass.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using ZenPlatform.Compiler.Contracts.Symbols;
+using ZenPlatform.Configuration.Structure.Data;
+using ZenPlatform.Configuration.Structure.Data.Types.Complex;
+using ZenPlatform.Language.Ast;
+using ZenPlatform.Language.Ast.Definitions;
+using ZenPlatform.Language.Ast.Definitions.Functions;
+
+namespace ZenPlatform.Configuration.Compiler
+{
+    public class ComponentAstBase : TypeEntity, IAstSymbol
+    {
+        public override T Accept<T>(AstVisitorBase<T> visitor)
+        {
+            return visitor.VisitTypeEntity(this);
+        }
+
+        public XCComponent Component { get; }
+
+        public XCObjectTypeBase Type { get; }
+
+        public ComponentAstBase(XCComponent component, XCObjectTypeBase type, ILineInfo lineInfo, string name) : base(
+            lineInfo, name)
+        {
+            Component = component;
+            Type = type;
+        }
+
+        public ComponentAstBase(XCComponent component, XCObjectTypeBase type, ILineInfo lineInfo, string name,
+            TypeBody tb) : base(lineInfo,
+            name, tb)
+        {
+            Component = component;
+            Type = type;
+        }
+
+        public SymbolScope SymbolScope { get; set; }
+    }
+
+    /// <summary>
+    /// Перенаправляет генерацию кода в компонент
+    /// </summary>
+    public class ComponentClass : ComponentAstBase
+    {
+        public ComponentClass(XCComponent component, XCObjectTypeBase type, ILineInfo lineInfo, string name) : base(
+            component, type, lineInfo, name)
+        {
+        }
+
+        public ComponentClass(XCComponent component, XCObjectTypeBase type, ILineInfo lineInfo, string name,
+            TypeBody tb) : base(component, type, lineInfo, name, tb)
+        {
+        }
+    }
+
+
+    /// <summary>
+    /// Перенаправляет генерацию кода в компонент
+    /// </summary>
+    public class ComponentModule : ComponentAstBase
+    {
+        public ComponentModule(XCComponent component, XCObjectTypeBase type, ILineInfo lineInfo, string name) : base(
+            component, type, lineInfo, name)
+        {
+        }
+
+        public ComponentModule(XCComponent component, XCObjectTypeBase type, ILineInfo lineInfo, string name,
+            TypeBody tb) : base(component, type, lineInfo, name, tb)
+        {
+        }
+    }
+}
