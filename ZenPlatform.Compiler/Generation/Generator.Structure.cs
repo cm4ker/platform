f5817304184d3edab4848389aff931bd625ca0b9
cm4ker <cm4ker@gmail.com>
2019-11-03 18:56:22 +0600

    [compiler] next

--- ZenPlatform.Compiler/Generation/Generator.Structure.cs
+++ ZenPlatform.Compiler/Generation/Generator.Structure.cs
@@ -82,14 +82,14 @@ namespace ZenPlatform.Compiler.Generation
                     {
                         var tco = PreBuildComponentClass(co);
                         AfterBuild(co, tco);
-                        co.Component.ComponentImpl.Generator.PatchType(co, tco);
+                        co.Component.ComponentImpl.Generator.Stage0(co, tco);
                         break;
                     }
                     case ComponentModule cm:
                     {
                         var tcm = PreBuildComponentModule(cm);
                         AfterBuild(cm, tcm);
-                        cm.Component.ComponentImpl.Generator.PatchType(cm, tcm);
+                        cm.Component.ComponentImpl.Generator.Stage0(cm, tcm);
                         break;
                     }
 
@@ -158,8 +158,47 @@ namespace ZenPlatform.Compiler.Generation
                         }
 
                         break;
-                    case ComponentClass cc:
-                    case ComponentModule cm:
+                    case ComponentAstBase cab:
+                        var tcab = _stage0[cab];
+                        cab.Component.ComponentImpl.Generator.Stage1(cab, tcab);
+
+                        foreach (var function in cab.TypeBody.Functions.FilterFunc(_mode))
+                        {
+                            var mf = PrebuildFunction(function, tcab, cab is ComponentClass);
+                            _stage1Methods.Add(function, mf);
+                            cab.TypeBody.SymbolTable.ConnectCodeObject(function, mf);
+                        }
+
+                        if (cab is ComponentModule)
+                            break;
+
+                        foreach (var property in cab.TypeBody.Properties)
+                        {
+                            var pp = PrebuildProperty(property, tcab);
+                            cab.TypeBody.SymbolTable.ConnectCodeObject(property, pp);
+                            _stage1Properties.Add(property, pp);
+                        }
+
+                        foreach (var field in cab.TypeBody.Fields)
+                        {
+                            var pf = PrebuildField(field, tcab);
+                            cab.TypeBody.SymbolTable.ConnectCodeObject(field, pf);
+                            _stage1Fields.Add(field, pf);
+                            ;
+                        }
+
+                        if (!cab.TypeBody.Constructors.Any())
+                        {
+                            cab.TypeBody.AddConstructor(Constructor.Default);
+                        }
+
+                        foreach (var constructor in cab.TypeBody.Constructors)
+                        {
+                            var pf = PrebuildConstructor(constructor, tcab);
+                            _stage1constructors.Add(constructor, pf);
+                            ;
+                        }
+
                         break;
 
                     default:
@@ -210,7 +249,7 @@ namespace ZenPlatform.Compiler.Generation
                     case ComponentClass cc:
                     case ComponentModule cm:
                         break;
-                    
+
                     default:
                         throw new Exception("The type entity not supported");
                 }
