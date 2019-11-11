5339f3e25f0311477c87b64440110539bf72d259
Nikita Zenkov <cmaker.xtal@hotmail.com>
2019-11-11 22:57:47 +0600

    stack machine

--- ZenPlatform.QueryBuilder/QueryMachine.cs
+++ ZenPlatform.QueryBuilder/QueryMachine.cs
@@ -1,7 +1,26 @@
+using System.Collections.Generic;
+
 namespace ZenPlatform.QueryBuilder
 {
     public class QueryMachine
     {
-        
+        public Stack<object> _syntaxStack;
+
+        public QueryMachine()
+        {
+            _syntaxStack = new Stack<object>();
+        }
+
+        public void ld_table()
+        {
+        }
+
+        public void @as()
+        {
+        }
+
+        public void st_query()
+        {
+        }
     }
 }
