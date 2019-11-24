125745e0b9ec4cfcb655d68626e96f9a102a324e
Oleg Masterskikh <masterskih.oleg@gmail.com>
2019-11-24 23:34:57 +0600

    have added configuration storage tests

---
+++ ZenPlatform.Core.Test/ConfigurationStorageTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+using ZenPlatform.Configuration;
+using ZenPlatform.Configuration.Structure;
+using ZenPlatform.ConfigurationExample;
+using ZenPlatform.Core.Configuration;
+using ZenPlatform.Core.Test.Configuration;
+using ZenPlatform.Data;
+
+namespace ZenPlatform.Core.Test
+{
+    public class ConfigurationStorageTest
+    {
+        [Fact]
+        public void SaveAndLoadCofiguration()
+        {
+            var configuration = Factory.CreateExampleConfiguration();
+
+            var storage = new XCTestMemoryStorage();
+
+            configuration.Save(storage);
+
+            var loadedConfiguration = XCRoot.Load(storage);
+
+            Assert.Equal(configuration.GetHash(), loadedConfiguration.GetHash());
+        }
+
+        [Fact]
+        public void DatabaseStorage()
+        {
+            var configuration = Factory.CreateExampleConfiguration();
+
+            throw new NotSupportedException("нужно разобратся как сделать этот тест универсальным и не зависищем от connectionString");
+            
+
+            using (var context = new DataContext(QueryBuilder.SqlDatabaseType.SqlServer, ""))
+            {
+
+                var storage = new XCDatabaseStorage("test", context);
+
+                configuration.Save(storage);
+
+                var loadedConfiguration = XCRoot.Load(storage);
+
+                Assert.Equal(configuration.GetHash(), loadedConfiguration.GetHash());
+            }
+            
+        }
+
+
+        [Fact]
+        public void FileSystemStorage()
+        {
+            var configuration = Factory.CreateExampleConfiguration();
+
+            var path = Directory.CreateDirectory(Path.GetTempFileName());
+
+
+            var storage = new XCFileSystemStorage(path.FullName, "Test");
+
+            configuration.Save(storage);
+
+            var loadedConfiguration = XCRoot.Load(storage);
+
+            Assert.Equal(configuration.GetHash(), loadedConfiguration.GetHash());
+        }
+
+
+    }
+}
