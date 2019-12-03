41f8e6b40d4788a8f4d582f73efb52be5bb52e33
cm4ker <cm4ker@gmail.com>
2019-12-03 22:22:56 +0600

    cecil is back

--- ZenPlatform.Compiler.Platform/XCCompiler.cs
+++ ZenPlatform.Compiler.Platform/XCCompiler.cs
@@ -24,7 +24,7 @@ namespace ZenPlatform.Compiler.Platform
 
         public IAssembly Build(IXCRoot configuration, CompilationMode mode, SqlDatabaseType targetDatabaseType)
         {
-            IAssemblyPlatform pl = new DnlibAssemblyPlatform();
+            IAssemblyPlatform pl = new CecilAssemblyPlatform();
             var assemblyBuilder = pl.CreateAssembly($"{configuration.ProjectName}{Enum.GetName(mode.GetType(), mode)}");
 
             var root = new Root(null, new List<CompilationUnit>());
