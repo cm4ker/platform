579a2ee8ea347accddbf627e26b297badc5ad187
cm4ker <cm4ker@gmail.com>
2019-12-04 14:45:50 +0600

    [wasm] add dom model

---
+++ wasm-work/WebAssembly.Browser/DOM/EventTarget.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using WebAssembly;
+using WebAssembly.Browser.DOM.Events;
+
+
+namespace WebAssembly.Browser.DOM
+{
+
+
+    [Export("EventTarget", typeof(JSObject))]
+    public partial class EventTarget : DOMObject, IEventTarget
+    {
+
+        static int nextEventId = 0;
+        static int NextEventId => nextEventId++;
+
+        public EventTarget(JSObject handle) : base(handle) { }
+
+        protected EventTarget(string globalName) : base(globalName)
+        {
+
+        }
+
+        internal Dictionary<string, DOMEventHandler> eventHandlers = new Dictionary<string, DOMEventHandler>();
+
+
+        [Export("addEventListener")]
+        public void AddEventListener(string type, DOMEventHandler listener, object options)
+        {
+
+            bool addNativeEventListener = false;
+            lock (eventHandlers)
+            {
+                if (!eventHandlers.ContainsKey(type))
+                {
+                    eventHandlers.Add(type, null);
+                    addNativeEventListener = true;
+                }
+                eventHandlers[type] += listener;
+            }
+
+            if (addNativeEventListener)
+            {
+
+                var UID = NextEventId;
+                AddJSEventListener(type, dispather(), UID);
+
+            }
+
+
+        }
+
+        public delegate int DOMEventDelegate(string eventType, string typeOfEvent, JSObject eventTarget, JSObject eventHandle,
+                      bool evAltKey,
+                      bool evBubbles,
+                      bool evCancelable,
+                      bool evCtrlKey,
+                      int evDetail,
+                      int evEventPhase,
+                      bool evMetaKey,
+                      bool evShitKey,
+                      int evKeyCode,
+                      int evPointerId,
+                      string evPointerType,
+                      double evScreenX,
+                      double evScreenY,
+                      double timeStamp,
+                      bool evIsTrusted,
+                      bool evScoped,
+                      double evClientX,
+                      double evClientY,
+                      int evButton,
+
+                      string eventInfo = null);
+
+
+        DOMEventDelegate dispather()
+        {
+            return new DOMEventDelegate(DispatchDOMEvent);
+        }
+
+        [Export("dispatchEvent")]
+        public bool DispatchEvent(Event evt)
+        {
+            return InvokeMethod<bool>("dispatchEvent", evt);
+        }
+
+        [Export("removeEventListener")]
+        public void RemoveEventListener(string type, DOMEventHandler listener, object options)
+        {
+
+        }
+
+    }
+
+}
---
+++ wasm-work/client/MainProgram.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using WebAssembly;
+using WebAssembly.Browser.DOM;
+
+public class Program
+{
+    public static void Main()
+    {
+        var document = new Document();
+             
+        // Create a new div element
+        var newDiv = document.CreateElement<HTMLDivElement>();
+
+        // and give it some content
+        var newContent = document.CreateTextNode("Hi there and greetings!");
+             
+        // add the text node to the newly created div
+        newDiv.AppendChild(newContent);
+
+        // add the newly created element and its content into the DOM 
+        var currentDiv = document.GetElementById("div1"); 
+
+        document.Body.InsertBefore(newDiv, currentDiv); 
+    }
+}
+
+public static class C
+{
+    public static int Add(int a, int b)
+    {
+        var g = (JSObject) Runtime.GetGlobalObject("document");
+
+        if (g == null)
+            throw new Exception("Это фиаско, братан!");
+
+        var c = (int) g.GetObjectProperty("test");
+
+        return a + b + c;
+    }
+}
