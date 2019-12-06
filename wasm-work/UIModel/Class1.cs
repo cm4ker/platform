da6c780983ea239e117b2f756b1988e79b124f07
cmaker <cm4ker@gmail.com>
2019-12-06 11:25:05 +0600

    [wasm] draft form

--- wasm-work/UIModel/Class1.cs
+++ wasm-work/UIModel/Class1.cs
@@ -1,8 +1,73 @@
 ﻿using System;
+using System.Linq;
 using WebAssembly.Browser.DOM;
 
 namespace UIModel
 {
+    
+    
+    
+    /*
+     
+     Нам нужно решить следующие проблемы:
+     
+     
+     1) Создание формы на сервере | Решается XML схемой (билдим там, передаем, запускаем)
+     2) Code Behind: нам нужно делать следующее
+     XML Form Representation + Some code represesntation
+     
+     
+     public class MyForm
+     {
+        private Node Element;
+        public void MyForm()
+        {
+            Init();
+        }     
+        
+        public void Init()
+        {
+            // Get XML
+            
+            Element = Service.Interpret(xml, this);
+        }
+     }
+
+     Interpret(xml , form)
+     {
+        // CreateElement.. var element = Node.....
+        if(Events != null)
+            if(xml.HasOnClick)
+            {
+                node.OnClick += Reflection.GetMethod(xml.OnClick.Name);
+            }
+     
+     }
+     
+     
+     <form> 
+     
+        <Element OnClick="Test">
+     
+     </form>
+     
+     [Client]
+     void Test()
+     {
+         //SomeCode
+     }
+     
+     [Server]
+     {
+        ///Some code
+        
+        //It will be wrapped to server call proc on the client
+        //and translate as is to the server assembly     
+     }
+     
+     
+     */
+    
     public abstract class Component
     {
         public virtual Node Element { get; }
@@ -25,7 +90,7 @@ namespace UIModel
             _underlyingElement.Type = InputElementType.Text;
             _underlyingElement.OnClick += (sender, args) =>
             {
-                ((HTMLInputElement) sender).Value = "HELLLO";
+                ((HTMLInputElement) sender).Value = this.GetType().GetProperties().First().GetMethod.Name;
             };
         }
 
--- wasm-work/download-latest.ps1
+++ wasm-work/download-latest.ps1
@@ -1,4 +1,4 @@
-Invoke-WebRequest -OutFile mono.wasm.zip https://jenkins.mono-project.com/job/test-mono-mainline-wasm/label=ubuntu-1804-amd64/lastSuccessfulBuild/Azure/processDownloadRequest/4312/ubuntu-1804-amd64/sdks/wasm/mono-wasm-42c5ccc298c.zip
+Invoke-WebRequest -OutFile mono.wasm.zip "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/4319/label=ubuntu-1804-amd64/Azure/processDownloadRequest/4319/ubuntu-1804-amd64/sdks/wasm/mono-wasm-a3fbb644938.zip"
 
 Expand-Archive -LiteralPath mono.wasm.zip -DestinationPath .\mono
 
