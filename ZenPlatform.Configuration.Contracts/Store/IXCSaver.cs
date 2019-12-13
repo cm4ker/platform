008d653c89f7b4ab2a376dba34a06adce413d3dd
Oleg Masterskikh <masterskih.oleg@gmail.com>
2019-12-13 17:19:31 +0600

    добавлены интерфейсы загрузки\сохранения конфигурации

---
+++ ZenPlatform.Configuration.Contracts/Store/IXCSaver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZenPlatform.Configuration.Contracts
+{
+    public interface IXCSaver
+    {
+        void Save<T>(string path, IXCConfigurationItem<T> item)
+            where
+            T : IXCSettingsItem;
+
+        void Save(string path, byte[] data);
+    }
+}
---
+++ ZenPlatform.Configuration/Storages/XCSaverLoader.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ZenPlatform.Configuration.Contracts;
+using ZenPlatform.Configuration.Structure;
+
+namespace ZenPlatform.Configuration.Storages
+{
+    public class XCSaverLoader : IXCLoader, IXCSaver
+    {
+
+        public XCSaverLoader(IXCConfigurationStorage storage)
+        {
+            _storage = storage;
+        }
+        public IXCConfigurationStorage _storage;
+        public T Load<T, C>(string path, bool loadTree = true)
+            where
+            T : IXCConfigurationItem<C>, new()
+            where
+            C : IXCSettingsItem
+        {
+            using (var stream = _storage.GetBlob("", path))
+            {
+                var config = XCHelper.DeserializeFromStream<C>(stream);
+
+
+                var result = new T();
+
+                if (loadTree)
+                    result.Initialize(this, config);
+
+                return result;
+            }
+
+        }
+
+        public byte[] Load(string path)
+        {
+            using (var stream = _storage.GetBlob("", path))
+            {
+
+                var memoryStream = new MemoryStream();
+                stream.CopyTo(memoryStream);
+                byte[] myBynary = memoryStream.ToArray();
+                return memoryStream.ToArray();
+
+            }
+        }
+
+        public void Save<T>(string path, IXCConfigurationItem<T> item)
+            where
+            T : IXCSettingsItem
+        {
+
+            var config = item.Store(this);
+
+            _storage.SaveBlob("", path, config.SerializeToStream());
+
+
+        }
+
+        public void Save(string path, byte[] data)
+        {
+            _storage.SaveBlob("", path, new MemoryStream(data));
+        }
+    }
+}
