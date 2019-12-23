3feb311d8c4da8e5ae1a660b8d02b41739b43ce9
cm4ker <cm4ker@gmail.com>
2019-12-23 15:47:35 +0600

    [client] wasm

---
+++ client-avalonia-work/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Avalonia;
+
+namespace client_avalonia_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .Start<MainWindow>();
+        }
+    }
+}
