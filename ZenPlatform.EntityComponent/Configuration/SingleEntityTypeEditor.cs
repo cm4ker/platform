6f2677af4d4c64257fe9300878fb8107a7b58e56
Oleg Masterskikh <masterskih.oleg@gmail.com>
2019-12-25 01:26:46 +0600

    Добавлен TypeEditor для типов данных конфигурации

--- ZenPlatform.EntityComponent/Configuration/SingleEntityTypeEditor.cs
+++ ZenPlatform.EntityComponent/Configuration/SingleEntityTypeEditor.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using ZenPlatform.Configuration.Contracts;
+using ZenPlatform.Configuration.Structure;
+using ZenPlatform.Shared.ParenChildCollection;
 
 namespace ZenPlatform.EntityComponent.Configuration
 {
@@ -15,9 +17,20 @@ namespace ZenPlatform.EntityComponent.Configuration
         public SingleEntityTypeEditor(IXCComponent component)
         {
             _metadata = new XCSingleEntityMetadata();
+
+            _metadata.EntityId = Guid.NewGuid();
+            _metadata.LinkId = Guid.NewGuid();
+
             _type = new XCSingleEntity(_metadata);
 
             _link = new XCSingleEntityLink(_type, _metadata);
+
+
+            ((IChildItem<IXCComponent>)_type).Parent = component;
+            ((IChildItem<IXCComponent>)_link).Parent = component;
+
+
+            
 
             _component = component;
 
@@ -31,6 +44,60 @@ namespace ZenPlatform.EntityComponent.Configuration
         public XCSingleEntity Type => _type;
 
         public XCSingleEntityLink Link => _link;
+
+        public SingleEntityTypeEditor SetName(string name)
+        {
+            _metadata.Name = name;
+            return this;
+        }
+
+        public SingleEntityTypeEditor SetDescription(string description)
+        {
+            
+            return this;
+        }
+
+        public SingleEntityTypeEditor SetId(Guid id)
+        {
+            _metadata.EntityId = id;
+            return this;
+        }
+
+        public SingleEntityTypeEditor SetLinkId(Guid id)
+        {
+            _metadata.LinkId = id;
+            return this;
+        }
+
+        public SingleEntityTypeEditor SetRealTableName(string tableName)
+        {
+            _type.RelTableName = tableName;
+            return this;
+        }
+
+        public SingleEntityPropertyEditor CreateProperty()
+        {
+            var newProperty = new XCSingleEntityProperty();
+            newProperty.Guid = Guid.NewGuid();
+            _metadata.Properties.Add(newProperty);
+
+            return new SingleEntityPropertyEditor(newProperty);
+        }
+
+        public SingleEntityModuleEditor CreateModule()
+        {
+            var module = new XCSingleEntityModule();
+            
+            _metadata.Modules.Add(module);
+            return new SingleEntityModuleEditor(module);
+        }
+
+        public SingleEntityCommandEditor CreateCommand()
+        {
+            var command = new XCCommand(true);
+            _metadata.Command.Add(command);
+            return new SingleEntityCommandEditor(command);
+        }
 
     }
 }
