0e6711ffe3545210afb2ff7e95ba9bc79c61b1b1
cm4ker <cm4ker@gmail.com>
2020-01-09 18:00:45 +0600

    EntityLink generator

--- ZenPlatform.EntityComponent/Entity/EntityLinkClassGenerator.cs
+++ ZenPlatform.EntityComponent/Entity/EntityLinkClassGenerator.cs
@@ -1,51 +1,77 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using Mono.Cecil.Cil;
-using MoreLinq.Extensions;
 using ZenPlatform.Compiler;
 using ZenPlatform.Compiler.Contracts;
 using ZenPlatform.Compiler.Contracts.Symbols;
 using ZenPlatform.Configuration.Contracts;
 using ZenPlatform.Configuration.Contracts.Data;
-using ZenPlatform.Configuration.Structure.Data.Types.Primitive;
 using ZenPlatform.EntityComponent.Configuration;
 using ZenPlatform.Language.Ast;
 using ZenPlatform.Language.Ast.Definitions;
-using ZenPlatform.Language.Ast.Definitions.Expressions;
-using ZenPlatform.Language.Ast.Definitions.Statements;
-using ZenPlatform.Language.Ast.Infrastructure;
 using ZenPlatform.QueryBuilder;
 using ZenPlatform.Shared.Tree;
 
 namespace ZenPlatform.EntityComponent.Entity
 {
-    public class EntityObjectClassGenerator
+    /*
+     
+     Link
+     {
+        ViewBag _vb;
+     
+        .ctor Link(ViewBag vb)
+        {
+            _vb = vb;
+            Init();            
+        }
+     
+        AnyProp     
+        public [Type] [PropName] { get; private set; }
+        
+        private void Init()
+        {
+            _vb.TrySet("Id", out Id);
+            _vb.TrySet("Name", out Name);
+            
+            ..ETC..
+            
+            SomeExternalLink = 
+        }
+        
+        public void Reload()
+        {
+            _vb = Manager.GetViewBag();
+            Init();
+        }        
+     }
+     
+     
+     */
+
+    public class EntityLinkClassGenerator
     {
         private readonly IXCComponent _component;
 
-        public EntityObjectClassGenerator(IXCComponent component)
+        public EntityLinkClassGenerator(IXCComponent component)
         {
             _component = component;
         }
 
-        public void GenerateAstTree(IXCObjectType type, Root root)
+        public void GenerateAstTree(IXCLinkType type, Root root)
         {
             var className = type.Name;
 
             var @namespace = _component.GetCodeRule(CodeGenRuleType.NamespaceRule).GetExpression();
 
-            var cls = new ComponentClass(CompilationMode.Server, _component, type, null, className,
-                new TypeBody(new List<Member>()));
-            cls.Bag = ObjectType.Object;
+            var cls = new ComponentClass(CompilationMode.Shared, _component, type, null, className,
+                new TypeBody(new List<Member>())) {Base = "Documents.EntityLink", Namespace = "Documents"};
 
             cls.Namespace = @namespace;
+            cls.Bag = ObjectType.Link;
 
-            GenerateObjectClassUserModules(type, cls);
 
             var cu = new CompilationUnit(null, new List<NamespaceBase>(), new List<TypeEntity>() {cls});
-            //end create dto class
             root.Add(cu);
         }
 
@@ -53,7 +79,7 @@ namespace ZenPlatform.EntityComponent.Entity
         {
             if (astTree is ComponentClass cc)
             {
-                if (cc.Bag != null && ((ObjectType) cc.Bag) == ObjectType.Object)
+                if (cc.Bag != null && ((ObjectType) cc.Bag) == ObjectType.Link)
                 {
                     if (cc.CompilationMode.HasFlag(CompilationMode.Server) && mode.HasFlag(CompilationMode.Server))
                     {
@@ -69,35 +95,31 @@ namespace ZenPlatform.EntityComponent.Entity
         private void EmitBody(ComponentClass cc, ITypeBuilder builder, SqlDatabaseType dbType)
         {
             var type = cc.Type;
-            var set = cc.Type as XCSingleEntity ?? throw new Exception("This component can generate only SingleEntity");
+            var set = cc.Type as XCSingleEntityLink ?? throw new Exception("This component can generate only SingleEntity");
             var ts = builder.Assembly.TypeSystem;
             var sb = ts.GetSystemBindings();
             var dtoClassName =
-                $"{_component.GetCodeRuleExpression(CodeGenRuleType.DtoPreffixRule)}{type.Name}{_component.GetCodeRuleExpression(CodeGenRuleType.DtoPostfixRule)}";
+                $"{_component.GetCodeRuleExpression(CodeGenRuleType.DtoPreffixRule)}{set.ParentType.Name}{_component.GetCodeRuleExpression(CodeGenRuleType.DtoPostfixRule)}";
 
 
             var @namespace = _component.GetCodeRule(CodeGenRuleType.NamespaceRule).GetExpression();
 
             var dtoType = ts.FindType($"{@namespace}.{dtoClassName}");
-            var session = ts.GetSystemBindings().Session;
-
-            var c = builder.DefineConstructor(false, dtoType, session);
+           
+            var c = builder.DefineConstructor(false, dtoType);
             var g = c.Generator;
 
             var dtoPrivate = builder.DefineField(dtoType, "_dto", false, false);
-            var sessionPrivate = builder.DefineField(session, "_session", false, false);
+            
 
             g.LdArg_0()
                 .EmitCall(builder.BaseType.FindConstructor())
                 .LdArg_0()
                 .LdArg(1)
                 .StFld(dtoPrivate)
-                .LdArg_0()
-                .LdArg(2)
-                .StFld(sessionPrivate)
                 .Ret();
 
-            foreach (var prop in set.Properties)
+            foreach (var prop in set.GetProperties())
             {
                 bool propertyGenerated = false;
 
@@ -108,7 +130,7 @@ namespace ZenPlatform.EntityComponent.Entity
                     : prop.Types[0].ConvertType(sb);
 
 
-                var propBuilder = builder.DefineProperty(propType, propName, true, !prop.IsReadOnly, false);
+                var propBuilder = builder.DefineProperty(propType, propName, true, false, false);
                 var getBuilder = propBuilder.getMethod.Generator;
                 var setBuilder = propBuilder.setMethod?.Generator;
 
@@ -158,31 +180,10 @@ namespace ZenPlatform.EntityComponent.Entity
                         getBuilder
                             .Ret()
                             .MarkLabel(label);
-
-
-                        if (!prop.IsReadOnly)
-                        {
-                            label = setBuilder.DefineLabel();
-                            //SETTER
-                            setBuilder
-                                .LdArg(1)
-                                .IsInst(compileType)
-                                .BrFalse(label)
-                                .LdArg_0()
-                                .LdFld(dtoPrivate)
-                                .LdArg(1)
-                                .Unbox_Any(compileType)
-                                .EmitCall(dtoProp.Setter)
-                                .LdArg_0()
-                                .LdFld(dtoPrivate)
-                                .LdcI4((int) ctype.Id)
-                                .EmitCall(dtoTypeProp.Setter)
-                                .MarkLabel(label);
-                        }
                     }
 
                     getBuilder.Throw(sb.Exception);
-                    setBuilder.Throw(sb.Exception);
+
 
                     // getBuilder.Ret();
                     // setBuilder.Ret();
