939e8838064cbe0d800ded3d991bfd4f2f9eabef
cm4ker <cm4ker@gmail.com>
2020-01-16 11:22:02 +0600

    [compiler] Small refactor

--- ZenPlatform.Compiler/Generation/Generator.EmitExpression.cs
+++ ZenPlatform.Compiler/Generation/Generator.EmitExpression.cs
@@ -321,7 +321,7 @@ namespace ZenPlatform.Compiler.Generation
             }
             else if (expression is GlobalVar gv)
             {
-                GlobalVarEmitter(e, _varManager.Root, gv.Expression, symbolTable);
+                EmitGlobalVar(e, _varManager.Root, gv.Expression, symbolTable);
 
                 /*
                  Глобальное адрессное пространство.
@@ -364,51 +364,6 @@ namespace ZenPlatform.Compiler.Generation
                  
                  */
             }
-        }
-
-        private Node GlobalVarEmitter(IEmitter e, Node currentGv, Expression expr, SymbolTable symbolTable)
-        {
-            if (expr is Name n)
-            {
-                var gv = currentGv.Childs.First(x =>
-                {
-                    var gvar = x as GlobalVarTreeItem;
-
-                    if (gvar?.Type == VarTreeLeafType.Prop)
-                        return gvar?.Name == n.Value;
-
-                    return false;
-                }) as GlobalVarTreeItem;
-
-                gv?.Emit(n, e);
-
-                return gv;
-            }
-            else if (expr is Call c)
-            {
-                var gv = currentGv.Childs.First(x =>
-                {
-                    var gvar = x as GlobalVarTreeItem;
-
-                    if (gvar?.Type == VarTreeLeafType.Func)
-                        return gvar?.Name == c.Name;
-
-                    return false;
-                }) as GlobalVarTreeItem;
-
-                EmitArguments(e, c.Arguments, symbolTable);
-
-                gv?.Emit(c, e);
-
-                return gv;
-            }
-            else if (expr is LookupExpression le)
-            {
-                var last = GlobalVarEmitter(e, currentGv, le.Current, symbolTable);
-                return GlobalVarEmitter(e, last, le.Lookup, symbolTable);
-            }
-            
-            throw new Exception("Unknown expression in global space");
         }
 
 
