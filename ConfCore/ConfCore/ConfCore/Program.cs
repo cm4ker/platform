9e3e83f5eefecdf8ac8757fcf8d1321a1e72e9dc
cm4ker <cm4ker@gmail.com>
2020-01-22 02:46:20 +0600

    [conf] new project create

--- ConfCore/ConfCore/ConfCore/Program.cs
+++ ConfCore/ConfCore/ConfCore/Program.cs
@@ -9,4 +9,61 @@ namespace ConfCore
             Console.WriteLine("Hello World!");
         }
     }
+
+
+    /*
+     CreateDataType -> Create MD -> Create derivative MD
+     
+     
+     _type
+     {
+        Id,            
+        ParentId,
+        
+        Name,
+        
+        IsRoot,
+        IsDerivative,
+        IsPrimitive,
+        
+        IsAbstract
+        IsLink
+     }
+     
+     _prop
+     {
+        Id,
+        Name,
+        OwnerTypeId     
+     }
+     
+     _prop_types
+     {
+        PropId,
+        TypeId,   
+        
+        Scale,
+        Precision,
+        Size                  
+     }
+          
+     */
+
+
+    public class MDType
+    {
+        public Guid Id { get; set; }
+        public Guid? ParentId { get; set; }
+
+
+        public string Name { get; set; }
+    }
+
+    public class MDProp
+    {
+    }
+
+    public class Configuration
+    {
+    }
 }
