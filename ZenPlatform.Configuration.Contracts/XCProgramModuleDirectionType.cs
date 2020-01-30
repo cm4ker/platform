023609e96b2b7c288f2797e6903d2e6fd52b8cba
cm4ker <cm4ker@gmail.com>
2020-01-31 01:22:53 +0600

    [conf] continue refactor

--- ZenPlatform.Configuration.Contracts/XCProgramModuleDirectionType.cs
+++ ZenPlatform.Configuration.Contracts/XCProgramModuleDirectionType.cs
@@ -20,4 +20,7 @@ namespace ZenPlatform.Configuration.Contracts
         /// </summary>
         ClientServer
     }
+    
+    
+    
 }
--- ZenPlatform.EntityComponent/Configuration/ComponentManager.cs
+++ ZenPlatform.EntityComponent/Configuration/ComponentManager.cs
@@ -17,53 +17,9 @@ using ZenPlatform.Language.Ast.Definitions.Statements;
 
 namespace ZenPlatform.EntityComponent.Configuration
 {
-    public static class FileSystemExtensions
-    {
-        public static T Deserialize<T>(this IFileSystem fs, string path)
-        {
-            try
-            {
-                using (var stream = fs.OpenFile(FileSystemPath.Parse(path), FileAccess.Read))
-                {
-                    return XCHelper.DeserializeFromStream<T>(stream);
-                }
-            }
-            catch
-            {
-                return default;
-            }
-        }
-
-        public static byte[] GetBytes(this IFileSystem fs, string path)
-        {
-            using (var stream = fs.OpenFile(FileSystemPath.Parse(path), FileAccess.Read))
-            {
-                var memoryStream = new MemoryStream();
-                stream.CopyTo(memoryStream);
-                return memoryStream.ToArray();
-            }
-        }
-
-        public static void Serialize(this IFileSystem fs, string path, object obj)
-        {
-            using (var stream = fs.OpenFile(FileSystemPath.Parse(path), FileAccess.Read))
-                obj.SerializeToStream().CopyTo(stream);
-        }
-
-        public static void SaveBytes(IFileSystem fs, string path, byte[] data)
-        {
-            using (var stream = fs.OpenFile(FileSystemPath.Parse(path), FileAccess.Read))
-                stream.Write(data, 0, data.Length);
-        }
-    }
-
     public class ComponentBuilder
     {
-        
-        
         public FileSystemPath Entry { get; set; }
-        
-        public void 
     }
 
     public class ObjectBuilder
@@ -171,15 +127,15 @@ namespace ZenPlatform.EntityComponent.Configuration
         }
     }
 
-    public class ComponentLoader : IComponentManager
+    public class ComponentManager : IComponentManager
     {
         private Assembly _asm;
 
         private Dictionary<string, object> _loadedObjects;
 
-        public ComponentLoader()
+        public ComponentManager()
         {
-            _asm = typeof(ComponentLoader).Assembly;
+            _asm = typeof(ComponentManager).Assembly;
         }
 
         /// <summary>
