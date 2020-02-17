2552a13d60599caaf8cc60f13596a9722823ac1a
cm4ker <cm4ker@gmail.com>
2020-02-17 21:48:28 +0600

    create flexible system for integrate external .net types to the platgform with discribe all interactions and operations

--- ZenPlatform.Language.Ast/Definitions/Block.cs
+++ ZenPlatform.Language.Ast/Definitions/Block.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using ZenPlatform.Compiler.Contracts.Symbols;
 using ZenPlatform.Language.Ast.AST;
+using ZenPlatform.Language.Ast.Definitions.Functions;
 using ZenPlatform.Language.Ast.Definitions.Statements;
 using ZenPlatform.Language.Ast.Infrastructure;
+using ZenPlatform.Shared.Tree;
 
 namespace ZenPlatform.Language.Ast.Definitions
 {
@@ -20,6 +24,54 @@ namespace ZenPlatform.Language.Ast.Definitions
                 return;
 
             Statements = statements;
+        }
+    }
+
+    public static partial class Factory
+    {
+        public static SyntaxCollectionNode<Statement> StatementsCollection(List<Statement> statements)
+        {
+
+        }
+    }
+
+    public class SyntaxCollectionNode<T> : SyntaxNode, IEnumerable<T> where T : SyntaxNode
+    {
+        public SyntaxCollectionNode(ILineInfo lineInfo) : base(lineInfo)
+        {
+        }
+
+        public override void Add(Node node)
+        {
+            if (!(node is Statement s))
+                throw new Exception("This is not allowed");
+
+            Add(s);
+        }
+
+        public void Add(Statement statement)
+        {
+            base.Add(statement);
+        }
+
+        public override T Accept<T>(AstVisitorBase<T> visitor)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var node in Childs)
+            {
+                var child = (T) node;
+
+                yield return child;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
