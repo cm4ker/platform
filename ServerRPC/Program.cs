7c1988b117127cea7782fbe215bb40feff4aab71
cm4ker <cm4ker@gmail.com>
2020-03-05 23:00:49 +0600

    thin client

--- ServerRPC/Program.cs
+++ ServerRPC/Program.cs
@@ -21,78 +21,20 @@ namespace ZenPlatform.ServerRPC
     {
         static void Main(string[] args)
         {
-            AssemblyDef d = new AssemblyDefUser("Test", Version.Parse("1.1.1.1"));
-            var m = new ModuleDefUser("Default");
-            d.Modules.Add(m);
+            DbConnection con = new SqlConnection();
 
-            var td = new TypeDefUser("A");
+            con.ConnectionString =
+                "Data source=PC702\\Asna; Initial catalog=asna_apt_019; User Id=sa; Password=sapwd123;"; // MultipleActiveResultSets=True";
 
-            var arrSig = new SZArraySig(m.CorLibTypes.Byte);
+            con.Open();
+
+            var cmd = con.CreateCommand();
+            cmd.CommandText = "select * from sys.tables";
+            var reader1 = cmd.ExecuteReader();
             
-            var md = new MethodDefUser("Test", MethodSig.CreateInstance(arrSig));
-            
-            md.Body = new CilBody();
-            td.Methods.Add(md);
-            
-            m.Types.Add(td);
-            
-            var g = md.Body;
-            
-            g.Instructions.Add(Instruction.CreateLdcI4(0));
-            g.Instructions.Add(Instruction.Create(OpCodes.Newarr, m.CorLibTypes.Byte));
-            g.Instructions.Add(Instruction.Create(OpCodes.Ret));
-
-            d.Write("MyAsm");
-
-
-//            Client client = new Client(new SimpleConsoleLogger<Client>());
-//
-//
-//            ClientPlatformContext clientPlatformContext = new ClientPlatformContext(null, client, null);
-//
-//            clientPlatformContext.Connect(new Core.Settings.DatabaseConnectionSettings()
-//                {Address = "127.0.0.1:12345", Database = "testdb"});
-//
-//            clientPlatformContext.Login("admin", "admin");
-
-
-            //platformClient.test();
-
-/*
-            client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345));
-            client.Use("test");
-
-            client.Authentication(new UserPasswordAuthenticationToken("admin", "admin"));
-            
-            //Start hack
-            Infrastructure.Main(client);
-
-            var asm = Assembly.LoadFile(AppDomain.CurrentDomain.BaseDirectory + "\\Debug.dll");
-            var type = asm.GetType("CompileNamespace.Test");
-            var mi = type.GetMethod("Add");
-            mi.Invoke(null, new object[] {2, 3});
-            //End hack
-           
-            var service = client.GetService<ITestProxyService>();
-
-            int res = service.Sum(10, 15);
-            Console.WriteLine($"res = {res}");
-
-            int i = client.Invoke<int>(new Route("test"), 44);
-
-            Console.WriteLine($"i = {i}");
-
-            using (var stream = client.InvokeStream(new Route("stream"), 44))
-            {
-                StreamReader reader = new StreamReader(stream);
-                var data = reader.ReadToEnd();
-
-                Console.WriteLine(data);
-            }
-
-
-            client.Close();
-*/
+            var cmd2 = con.CreateCommand();
+            cmd2.CommandText = "SElect * from sys.tables";
+            var reader2 = cmd2.ExecuteReader();
         }
     }
 }
