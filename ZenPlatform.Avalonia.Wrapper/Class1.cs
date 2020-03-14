de08fa5918854c8454ac8404fc2bbe55a1a72ac0
Nikita Zenkov <cmaker.xtal@hotmail.com>
2020-03-14 23:41:28 +0600

    [gen] prop gen getter

--- ZenPlatform.Avalonia.Wrapper/Class1.cs
+++ ZenPlatform.Avalonia.Wrapper/Class1.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Layout;
 using Avalonia.OpenGL;
 using Avalonia.Styling;
 using JetBrains.Annotations;
@@ -8,21 +11,34 @@ using JetBrains.Annotations;
 namespace ZenPlatform.Avalonia.Wrapper
 {
     /*
-     
-     Transformer.Wrap(string) => object
-     Transformer.Unwrap(UIElement) => string
-     
-     
-     */
+        Transformer.Wrap(string) => object
+        Transformer.Unwrap(UIElement) => string
+    */
 
     public static class Transformer
     {
-        public static UXElement Wrap;
+        public static UXElement Wrap()
+        {
+            var form = new UXForm();
+            var tb = new UXTextBox();
+
+            form.SetContent(tb);
+
+            return form;
+        }
     }
 
+    /*
+     <UXForm> 
+        <UXGroup Orient="Horizontal">
+            <UXTextBox />
+            <UXTextBox />
+        </UXGroup> 
+     </UXForm>
+     */
     public abstract class UXElement
     {
-        public abstract IControl GetUnderlyingControl();
+        public abstract Control GetUnderlyingControl();
     }
 
     public class UXForm : UXElement
@@ -36,12 +52,67 @@ namespace ZenPlatform.Avalonia.Wrapper
 
         public void SetContent(UXElement element)
         {
-            _uc.conelement.GetUnderlyingControl()
+            _uc.Content = element.GetUnderlyingControl();
         }
 
-        public override IControl GetUnderlyingControl()
+        public override Control GetUnderlyingControl()
         {
             return _uc;
+        }
+    }
+
+    public enum UXGroupOrientation
+    {
+        Horizontal,
+        Vertical
+    }
+
+    public class UXGroup : UXElement
+    {
+        private Grid _g;
+        private List<UXElement> _elements;
+
+        public UXGroup()
+        {
+            _elements = new List<UXElement>();
+            _g = new Grid();
+        }
+
+        public UXGroupOrientation Orientation { get; set; }
+
+        private void UpdateOrientation()
+        {
+            foreach (var element in _elements)
+            {
+            }
+        }
+
+        public void Add(UXElement element)
+        {
+            _elements.Add(element);
+
+            var underlyingControl = element.GetUnderlyingControl();
+
+            var index = _elements.Count - 1;
+
+
+            if (Orientation == UXGroupOrientation.Horizontal)
+            {
+                _g.ColumnDefinitions.Add(new ColumnDefinition());
+                Grid.SetColumn(underlyingControl, index);
+            }
+            else
+            {
+                _g.RowDefinitions.Add(new RowDefinition());
+                Grid.SetRow(underlyingControl, index);
+            }
+
+            _g.Children.Add(underlyingControl);
+        }
+
+        public override Control GetUnderlyingControl()
+        {
+            return _g;
         }
     }
 
@@ -54,7 +125,7 @@ namespace ZenPlatform.Avalonia.Wrapper
             _c = new TextBox();
         }
 
-        public override IControl GetUnderlyingControl()
+        public override Control GetUnderlyingControl()
         {
             return _c;
         }
