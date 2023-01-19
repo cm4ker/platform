f83364517310278f19c2914a38b7129fc15b4c6b
cm4ker <cm4ker@gmail.com>
2023-01-20 03:14:59 +0600

    fix deploy script and add red text on view

--- Build/library/deploy.cmd
+++ Build/library/deploy.cmd
@@ -1,4 +1,4 @@
-﻿dotnet build /flp:v=diag; /bl:binarylogfilename.binlog
+﻿dotnet build /t:Clean,Build /flp:v=diag; /bl:binarylogfilename.binlog
 aq deploy -pkg bin\Debug\net6.0\Package\library.aqpk -e localhost:5000 -i library
 timeout 2
 aq migrate -e localhost:5000 -i library
